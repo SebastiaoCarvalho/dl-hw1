fix gradient descent

skeleton_code/hw1-q1.py
@@ -141,7 +141,9 @@ class MLP(object):
             #print("z2", np.shape(z2))
             x2 = self.softmax(z2)
             #print("x2", np.shape(x2))
-            delta2 = x2 - y_i
+            e_y = np.zeros(x2.shape)
+            e_y[y_i] = 1
+            delta2 = x2 - e_y
             #print("delta2", np.shape(delta2))
             derivative1 = np.array(list(map(lambda x: list(map(lambda k : 1 if k > 0 else 0, x)), z1)))
             #print("derivative1", np.shape(derivative1))
@@ -151,13 +153,11 @@ class MLP(object):
             delta_b2 +=  delta2
             delta_W1 +=  np.dot(delta1, x_i.T)
             delta_b1 += delta1
-            e_y = np.zeros(x2.shape)
-            e_y[y_i] = 1
-            loss += -np.sum(e_y * np.log(x2))  
-        self.W2 -= learning_rate * delta_W2 / len(X)
-        self.W1 -= learning_rate * delta_W1 / len(X)
-        self.b2 -= learning_rate * delta_b2 / len(X)
-        self.b1 -= learning_rate * delta_b1 / len(X)
+            loss += -np.sum(e_y * np.log(x2))
+            self.W2 -= learning_rate * delta_W2
+            self.W1 -= learning_rate * delta_W1
+            self.b2 -= learning_rate * delta_b2
+            self.b1 -= learning_rate * delta_b1
         return loss/len(X)
 
 def plot(epochs, train_accs, val_accs):